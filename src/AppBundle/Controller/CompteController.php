57563bf849a675044223c6c9c8a3ccf76a6670f3
john2501 <jbr.legolito@gmail.com>
2017-08-25 15:36:08 +0200

    V1.3

--- src/AppBundle/Controller/CompteController.php
+++ src/AppBundle/Controller/CompteController.php
@@ -2,8 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\RegistrationType;
+use AppBundle\Form\UpdateCompte;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -27,10 +30,52 @@ class CompteController extends Controller
                 'user'=>$user,
             ]);
     }
+    /**
+     * @Route("/CompteUpdate/{id}", name="Update")
+     */
+    public function SetCompteAction($id, Request $request){
 
-    public function SetCompteAction($id){
+        $user=$this->getUser();
+        $form=$this->createForm(UpdateCompte::class,$user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            //Persister l'objet
+            $em=$this->getDoctrine()->getManager();
 
 
+
+            $em->persist($user);
+            $em->flush();
+
+
+            //rediriger vers la page home
+            return $this->redirectToRoute("homepage");
+        }
+
+
+        return $this->render("inscription.html.twig",
+            [
+                "form"=>$form->createView()
+            ]
+        );
+    }
+
+
+
+    /**
+     * @Route("/CompteDelete/{id}", name="Delete")
+     */
+    public function DeleteUserAction($id){
+        //$user=$this->getDoctrine()->getRepository("AppBundle:User")->findBy(array('id' => $id));
+$user=$this->getUser();
+        $em=$this->getDoctrine()->getManager();
+        $em->remove($user);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('homepage'));
 
     }
+
 }
