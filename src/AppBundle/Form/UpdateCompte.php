53c451f75e0a4a9475fe765f81a06599814f2641
john2501 <jbr.legolito@gmail.com>
2017-08-25 15:37:52 +0200

    V1.3

---
+++ src/AppBundle/Form/UpdateCompte.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UpdateCompte extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('email')
+            ->add("valider",SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User'
+        ));
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_bundle_update_compte';
+    }
+}
